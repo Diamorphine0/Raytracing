b8d76ce63ed2e45a97db099dd0faf8605f2d9bef
AlexanderStorozhenko <140406881+AlexanderStorozhenko@users.noreply.github.com>
2023-11-30 11:39:18 +0100

    Update main.cpp
    * For now to import the vertexshader and fragmentshader in the main file you have to pass the directory local to your machine (i.e initially set to "/Users/alexanderstorozhenko/EngineProject/vertexshader.shader"

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ using namespace glm;
 #include <shader.h>
 #include <random>
 
-glm::mat4 construct_mvp(int width, int height, glm::vec3 &position, glm::vec3 &direction, glm::vec3 &right, float verticalAngle, float horizontalAngle, float deltaTime, float speed, float initialFoV){
+// Tasks:
+// Make the movement around the screen more dynamic. (Job for someone)
+// Model Loading (Load .obj files)
+// Rotation of Objects using Quaternions.
+// Take care of conan package maanger.
+
+glm::mat4 construct_mvp(glm::vec3 &position, glm::vec3 &direction, glm::vec3 &right, float initialFoV){
 
     // Up vector : perpendicular to both direction and right
     glm::vec3 up = glm::cross(right, direction);
@@ -64,6 +70,12 @@ int main()
         return -1;
     }
 
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
+    // Change the shader path names
     GLuint programID = LoadShaders( "/Users/alexanderstorozhenko/EngineProject/vertexshader.shader", "/Users/alexanderstorozhenko/EngineProject/fragmentshader.shader" );
 
     // Ensure we can capture the escape key being pressed below
@@ -114,6 +126,45 @@ int main()
         1.0f,-1.0f, 1.0f
     };
 
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };
+
     // This will identify our vertex buffer
     GLuint vertexbuffer;
     // Generate 1 buffer, put the resulting identifier in vertexbuffer
@@ -122,6 +173,11 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     // position
     glm::vec3 position = glm::vec3( 0, 0, 5 );
@@ -136,9 +192,9 @@ int main()
     float lastTime;
     float deltaTime;
 
-    double xpos, ypos;
+    double xpos_last, ypos_last, xpos, ypos;
 
-    float speed = 3.0f; // 3 units / second
+    float speed = 0.001f; // 0.01 units / second
     float mouseSpeed = 0.0005f;
 
     do{
@@ -150,12 +206,15 @@ int main()
         deltaTime = float(currentTime - lastTime);
 
         // Get mouse position
+        xpos_last = xpos;
+        ypos_last = ypos;
+
         glfwGetCursorPos(window, &xpos, &ypos);
         // Reset mouse position for next frame
-        glfwSetCursorPos(window, (double) 1024/2, (double) 768/2);
+//        glfwSetCursorPos(window, (double) width/2, (double) height/2);
 
-        horizontalAngle += mouseSpeed * deltaTime * float(1024/2 - xpos );
-        verticalAngle   += mouseSpeed * deltaTime * float( 768/2 - ypos );
+        horizontalAngle += mouseSpeed * deltaTime * float(xpos_last - xpos );
+        verticalAngle   += mouseSpeed * deltaTime * float(ypos_last - ypos );
 
         glm::vec3 direction(
             cos(verticalAngle) * sin(horizontalAngle),
@@ -187,7 +246,7 @@ int main()
             position -= right * deltaTime * speed;
         }
 
-        glm::mat4 mvp = construct_mvp(width, height, position, direction, right, verticalAngle, horizontalAngle, deltaTime, speed, initialFoV);
+        glm::mat4 mvp = construct_mvp(position, direction, right, initialFoV);
 
         GLuint MatrixID = glGetUniformLocation(programID, "MVP");
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
@@ -202,6 +261,17 @@ int main()
             GL_FALSE,           // normalized?
             0,                  // stride
             (void*)0            // array buffer offset
+            );
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+            1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+            3,                                // size
+            GL_FLOAT,                         // type
+            GL_FALSE,                         // normalized?
+            0,                                // stride
+            (void*)0                          // array buffer offset
             );
 
         // Draw the cube !
