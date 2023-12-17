6e0fd8408ebad85179d3b748175b463969e3827d
davidkraus7 <dada.kraus@gmail.com>
2023-12-17 18:30:52 +0100

    Update ImageRenderer.cpp

--- ImageRenderer.cpp
+++ ImageRenderer.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "ImageRenderer.h"
+#include "GLFW/glfw3.h"
+
 #include "imgui/imgui.h"
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
@@ -44,11 +46,67 @@ bool ImageRenderer::set_all_pixels(const std::vector<std::vector<Color>> &colors
 
 void ImageRenderer::render_image() {
 
-    for(int i = 0; i < image_height; i++){
-        for(int j = 0; j < image_width; j++){
-        }
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
     }
 
+    // Set OpenGL version to 3.3 and use the core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(image_width, image_height, "Rendered Image", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Set the viewport size
+    glViewport(0, 0, image_width, image_height);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        // Loop through each pixel and set its color
+        // This is not the way to go, we should instead create a texture object first from our pixel_colors vector, and then display this texture object on the window
+
+        //replace from...
+
+        // Clear the framebuffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        for (int i = 0; i < image_height; ++i) {
+            for (int j = 0; j < image_width; ++j) {
+                glBegin(GL_POINTS);
+
+                // Set the color of the pixel
+                glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+                // Draw the pixel at normalized coordinates
+                glVertex2f(-1.0f + 2.0f * j / image_width, -1.0f + 2.0f * i / image_height);
+
+                glEnd();
+            }
+        }
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        //replace until...
+
+        // Check for window close events
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS || glfwWindowShouldClose(window))
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 }
 
 
