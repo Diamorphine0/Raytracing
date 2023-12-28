eb228b9754f17bf41cfadc335c2e18421468169d
sfeilaryan <sfeilaryan@gmail.com>
2023-12-28 18:40:51 +0200

    Update Metal.h

--- Metal.h
+++ Metal.h
@@ -7,9 +7,11 @@
 class Metal : public Material
 {
 public:
-    Metal(Color att) {attenuation = att;}
+    Metal(Color att, double smooth) {attenuation = att;smoothness = smooth;}
 
     virtual Ray scatter(const Ray&, Vec3, Vec3);
+
+    double smoothness;
 
 protected:
 
