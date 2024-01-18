327f5abfb086d44c79951a7571967dcfeb2bde53
sfeilaryan <sfeilaryan@gmail.com>
2024-01-18 16:31:56 +0100

    To create direction biased light

---
+++ collimatedlight.cpp
@@ -0,0 +1,6 @@
+#include "collimatedlight.h"
+
+CollimatedLight::CollimatedLight()
+{
+
+}
---
+++ collimatedlight.h
@@ -0,0 +1,13 @@
+#ifndef COLLIMATEDLIGHT_H
+#define COLLIMATEDLIGHT_H
+
+#include "Utilities.hpp"
+#include "Material.h"
+
+class CollimatedLight
+{
+public:
+    CollimatedLight();
+};
+
+#endif // COLLIMATEDLIGHT_H
